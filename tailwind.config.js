db86f6263c7f5586001c6a849af5f426a9edf32e
Riki Awaludin <rikiawaludin17@gmail.com>
2024-06-11 11:55:15 +0700

    migrate to tailwindcss cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
+  content: ['./public/app1.html'],
+  darkMode: 'class',
+      theme: {
+        extend: {
+          spacing: {
+            13: "3.25rem",
+          },
+          fontFamily: {
+            inter: ["Inter"],
+          },
+          animation: {
+            'spin-slow': 'spin 3s linear infinite',
+            'goyang': 'goyang 1s ease-in-out infinite'
+          },
+          keyframes: {
+            goyang: {
+              '0%, 100%': { transform: 'rotate(-9deg)' },
+              '50%': { transform: 'rotate(9deg)' }
+            }
+          }
+        },
+      },
   plugins: [],
 }
 
